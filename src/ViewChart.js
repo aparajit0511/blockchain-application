e9024cb8117d7ec14e91022c667a899de84c08ef
aparajit0511 <aparajit35@gmail.com>
2022-10-29 21:49:33 +0530

    package.json update2

--- src/ViewChart.js
+++ src/ViewChart.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
-import { mockComponent } from "react-dom/test-utils";
+
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 import moment from "moment";
